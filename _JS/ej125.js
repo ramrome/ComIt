a9d1aa4d51505fa993f6a8f2033989f5eb43bf89
ramrome <mariano-gsmk@hotmail.com>
2017-11-03 04:44:15 -0200

    nuevoCommit

---
+++ _JS/ej125.js
@@ -0,0 +1,3 @@
+let gustosDeHeados = ["Primero Chocolate" , "Segundo Sambayon" , "Tercero Crema Americana" , "Cuarto Frutilla" , "Cinco Dulce de leche" , "Seis Banana Split" , "Siete Almendra" , "Ocho Mascarpone" , " Nueve Cereza" , "Diez Limon"]
+
+console.log(gustosDeHeados)
---
+++ _JS/ej126.js
@@ -0,0 +1,12 @@
+let gustosDeHeados = ["Primero Chocolate" , "Segundo Sambayon" , "Tercero Crema Americana" , "Cuarto Frutilla" 
+, "Cinco Dulce de leche" , "Seis Banana Split" , "Siete Almendra" , "Ocho Mascarpone" , "Nueve Cereza" , "Diez Limon"];
+
+function nuevaLista(arrHelados , nuevoGusto) {
+	let newCream = arrHelados.reverse()
+		newCream.push (nuevoGusto)
+		return nuevoGusto
+
+}
+
+
+console.log(nuevaLista(gustosDeHeados, "Once Durazno" , "Doce Melon"))
